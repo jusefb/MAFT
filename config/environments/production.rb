252244b07eb5ea0ced35cb0b409ccdb1ac0a9bec
Jusef <jusef.belkasemi@gmail.com>
2013-09-08 20:05:43 +0100

    Merge branch 'master' of https://github.com/jusefb/MAFT
    Conflicts:
    	.idea/MAFT.iml
    	.idea/dataSources.ids
    	.idea/dataSources.xml
    	.idea/workspace.xml
    	Gemfile
    	Gemfile.lock
    	app/controllers/application_controller.rb
    	config/database.yml
    	coverage/.last_run.json
    	log/development.log
    	log/production.log
    	log/test.log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ MAFT::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -19,9 +19,6 @@ MAFT::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  # This is to fix the issue with images not precompiling
-  config.assets.initialize_on_precompile = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
